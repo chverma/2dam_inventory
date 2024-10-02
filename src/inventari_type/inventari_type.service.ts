10d024cce5e51d67c37ee49fed66557721c39016
David <davidmunteanonti@gmail.com>
2024-10-03 01:53:24 +0200

    Actualizacion API, introduccion funcion para devolver XML o JSON David Muntean

--- src/inventari_type/inventari_type.service.ts
+++ src/inventari_type/inventari_type.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import * as fs from 'node:fs';
 import * as path from 'path';
+import * as convert from 'xml-js';
 
 const filePath = path.join(
   path.resolve(__dirname, '..'),
@@ -11,10 +12,19 @@ const inventoryTypeData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 function saveData() {
   fs.writeFileSync(filePath, JSON.stringify(inventoryTypeData));
 }
+
 @Injectable()
 export class InventariTypeService {
-  getAllInventariType() {
-    return inventoryTypeData;
+  getAllInventariType(xml?: string) {
+    if (xml === 'true') {
+      const jsonformatted = { inventory_type: inventoryTypeData };
+      const json = JSON.stringify(jsonformatted);
+      const options = { compact: true, ignoreComment: true, spaces: 4 };
+      const result = convert.json2xml(json, options);
+      return result;
+    } else {
+      return inventoryTypeData;
+    }
   }
 
   createInventariType(inventari_type: any) {
@@ -26,13 +36,25 @@ export class InventariTypeService {
     return { message: 'Estado creado satisfactoriamente' };
   }
 
-  getInventariType(id: number) {
+  getInventariType(id: number, xml?: string) {
     let i = 0;
     while (i < inventoryTypeData.length && inventoryTypeData[i].id_type != id) {
       i++;
     }
-    if (inventoryTypeData[i]) return inventoryTypeData[i];
-    else throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    if (inventoryTypeData[i]) {
+      const result = inventoryTypeData[i];
+      if (xml === 'true') {
+        const jsonformatted = { inventory_type: result };
+        const json = JSON.stringify(jsonformatted);
+        const options = { compact: true, ignoreComment: true, spaces: 4 };
+        const xmlResult = convert.json2xml(json, options);
+        return xmlResult;
+      }else{
+        return result;
+      }
+    } else {
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
   }
 
   updateInventariType(inventariTypeUpdated) {
